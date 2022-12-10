a717113f0b0ec81c87906ffa742746d9c309b980
Robert <r.biel@posteo.de>
2022-12-10 14:30:35 +0100

    setup gh-pagesC

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          App.js edited and saved.
+          App.js edited and saved.</br>
+          Noch etwas mehr Text.
         </p>
         <a
           className="App-link"
